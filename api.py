3cb2f78b9248d23e4bd70cf94e405cf378750935
Elizabeth <2988690200101@ingenieria.usac.edu.gt>
2023-02-15 02:31:01 -0600

    Creación de EndPoint Sumar
    Se importó el objeto numeros y se crea un end point de tipo Post llamado sumar que se encarga de sumar dos numero extraidos de un JSON y retornar el resultado correspondiente

--- api.py
+++ api.py
@@ -1,6 +1,14 @@
 from flask import Flask, request, render_template
+from numeros import numeros
 import time, json, socket
 app= Flask(__name__)
 
+numeros = []
+@app.route("/sumar", methods=['POST'])
+def sumar_numeros():
+    resultado = int(request.json['num1'])+ int(request.json['num2'])
+    print(resultado)
+    return str(resultado)
+    
 if __name__=="__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)

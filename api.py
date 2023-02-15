081566e0f42814579314230cc7a2f91e4f1f58ef
Elizabeth <2988690200101@ingenieria.usac.edu.gt>
2023-02-15 03:06:02 -0600

    Creación de EndPoint Info
    Se creo un end point de tipo Get que retorna la información relevante del estudiante como su nombre y número de carnet

--- api.py
+++ api.py
@@ -9,6 +9,12 @@ def sumar_numeros():
     resultado = int(request.json['num1'])+ int(request.json['num2'])
     print(resultado)
     return str(resultado)
+
+@app.route("/", methods=['GET'])
+def mostrarInfo():
+    mensaje = "Caren Elizabeth Garcia Perez 201504119"
+    print("201504119")
+    return mensaje
     
 if __name__=="__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)
